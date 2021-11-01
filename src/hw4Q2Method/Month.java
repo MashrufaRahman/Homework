1f946ab48af67cdca8f638224746a3c7edf6195f
MashrufaRahman <mashrfrhmn@gmail.com>
2021-11-01 18:21:10 -0400

    11/01/2021 6:20pm

---
+++ src/hw4Q2Method/Month.java
@@ -0,0 +1,61 @@
+package hw4Q2Method;
+
+/*
+ * 02) Create a package name "hw4Q2Method" in your "HomeWorkOctober2021" project. Inside the package, a) Create a class "Month". Declare one variable -- String monthOfBirth = "YourMonthOfBirthHere" ; . Use switch method. as case use all the months in year and also default to print. Finally execute, your month of Birth as outcome in console. Organize your code. Push in the github. Paste your github link below. (If you are not able to push to github)
+ */
+
+public class Month {
+
+	public static void main(String[] args) {
+		String MonthOfBirth = "September";
+
+		switch (MonthOfBirth) {
+		
+		case "October":
+			System.out.println("This is your months of Birth:"+MonthOfBirth);
+			break;
+
+		case "November":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+
+		case "December":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "January":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+		case "February":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "March":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "April":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "May":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+
+		case "June":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "July":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+		case "August":
+			System.out.println("This is your months of birth"+MonthOfBirth);
+			break;
+
+		case "September":
+			System.out.println("This is your months of Birth:" +MonthOfBirth );
+			break;
+
+		default:
+			System.out.println("Finally execute, your month of Birth");
+			break;
+		}
+
+	}
+
+}
